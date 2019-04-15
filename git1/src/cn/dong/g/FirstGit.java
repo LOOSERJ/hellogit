0531411d2e0cf052ff6a9bbb67fd95810edc63c2
dong <1309001881@qq.com>
2019-04-15 20:55:00 +0800

    hello,git

--- git1/src/cn/dong/g/FirstGit.java
+++ git1/src/cn/dong/g/FirstGit.java
@@ -1,7 +1,13 @@
 package cn.dong.g;
 
 public class FirstGit {
+
     public static void main(String[] args) {
-        System.out.println("hello,spring");
+        System.out.println("hello");
     }
+
+    public void test(){
+        System.out.println("scuuess");
+    }
+
 }
